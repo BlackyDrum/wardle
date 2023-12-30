70dc66b34fd348eaa07107801efd2a934bbd2925
BlackyDrum <ganiaytan1010@web.de>
2023-12-30 22:01:44 +0100

    Add header

---
+++ app/components/Header.tsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <main>
+      <div className="text-center font-bold mt-3 mb-4 text-3xl">Wordle</div>
+      <hr className="h-px border-0 bg-app-gray"></hr>
+    </main>
+  );
+}
