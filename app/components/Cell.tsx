e304e1b29b5465f436e1174bb02ae2282f3cc9c9
BlackyDrum <ganiaytan1010@web.de>
2023-12-31 01:01:24 +0100

    Use char instead of array to save a cells character

--- app/components/Cell.tsx
+++ app/components/Cell.tsx
@@ -1,17 +1,17 @@
 import { useEffect, useState } from "react";
 
 export default function Cell(props: { char: string; activeRow: number; cellRow: number }) {
-  const [chars, setChars] = useState<string[]>([]);
+  const [char, setChar] = useState("");
 
   useEffect(() => {
     if (props.activeRow === props.cellRow) {
-      setChars((prevChars) => [...prevChars, props.char]);
+      setChar(props.char);
     }
   }, [props.activeRow, props.cellRow, props.char]);
 
   return (
     <div className="flex m-1 w-16 h-16 text-3xl border-app-gray border-2">
-      <div className="m-auto font-bold">{chars.map((char) => char)}</div>
+      <div className="m-auto font-bold">{char}</div>
     </div>
   );
 }
