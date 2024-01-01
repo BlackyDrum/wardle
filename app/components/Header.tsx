b9ca8c71959b1a6e16a3a8e1c85238837f176f27
BlackyDrum <ganiaytan1010@web.de>
2024-01-01 01:14:28 +0100

    Change app name from header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <header>
-      <div className="text-center font-bold mt-3 mb-4 text-3xl">Wordle</div>
+      <div className="text-center font-bold mt-3 mb-4 text-3xl">Wardle</div>
       <hr className="h-px border-0 bg-app-gray"></hr>
     </header>
   );
